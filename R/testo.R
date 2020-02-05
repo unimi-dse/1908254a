2618cd732ad4350b61e3fdc142443fd9d9c1a2f6
martinaviggiano <60003031+martinaviggiano@users.noreply.github.com>
2020-02-06 00:29:36 +0100

    Update functions

--- R/testo.R
+++ R/testo.R
@@ -1,8 +1,8 @@
 #install.packages('devtools')
 #install.packages("ggplot2")
 #install.packages("gmodels")
-#library(devtools)
-#library(ggplot2)
+library(devtools)
+library(ggplot2)
 #library(gmodels)
 
 mydata<-read.csv2('TelefMob.csv',header = TRUE,sep = ";",dec=",",na.strings = "nd")
@@ -10,8 +10,36 @@ dim(mydata)
 colnames(mydata)
 colnames(mydata)<- c("RatePlan", "Status", "Lifespan_days", "LastCallCenter_days", "LastTopUp_days", "RetailChannel","Minutes_Feb","Minutes_Gen", "Minutes_Dic", "Video_Feb", "Video_Gen", "Video_Dic", "SMS_Feb","SMS_Gen", "SMS_Dic", "Web_Feb", "Web_Gen", "Web_Dic" )
 
-sapply(mydata,function(x)sum(is.na(x)))  # apply a function over a vector
-mydata<-mydata[complete.cases(mydata),]  # vector without missing values
+#sapply(mydata,function(x)sum(is.na(x)))  # apply a function over a vector
+#' Missing values in mydata
+#'
+#' @return Number of missing values for each column
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'    sapplymydata()
+#' }
+sapplymydata<-function()
+{
+  sapply(mydata,function(x)sum(is.na(x)))  # apply a function over a vector
+}
+
+#mydata<-mydata[complete.cases(mydata),]  # vector without missing values
+#' Complete cases in mydata
+#'
+#' @return Complete mydata
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'    completemydata()
+#' }
+completemydata<-function()
+{
+  mydata<-mydata[complete.cases(mydata),]
+}
+
 dim(mydata) # 20 observations less
 
 summary(mydata$Status)
@@ -19,14 +47,42 @@ table(mydata$Status)
 
 str(mydata)
 
-mydata$Status<-as.factor(mydata$Status)
+##mydata$Status<-as.factor(mydata$Status)
+#' As factor
+#'
+#' @return Encode Status vector as a factor
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'    asfactormydata()
+#' }
+asfactormydata<-function()
+{
+  mydata$Status<-as.factor(mydata$Status)
+}
+
 table(mydata$Status)
 levels(mydata$Status)<-c("ON","OFF")
-palette<-c("royalblue","lightblue","cornflowerblue","lightskyblue")
+##palette<-c("royalblue","lightblue","cornflowerblue","lightskyblue")
+#' Palette
+#'
+#' @return Palette
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'    palette()
+#' }
+palette<-function()
+{
+  palette<-c("royalblue","lightblue","cornflowerblue","lightskyblue")
+}
 
-## Variable Status
+### Variable Status ###
+
 table1<-table(mydata$Status)
-plot(mydata$Status, xlab="Status of the SIM", ylab="Frequence", main= "Variable Status of the SIM", col=palette)
+##plot(mydata$Status, xlab="Status of the SIM", ylab="Frequence", main= "Variable Status of the SIM", col=palette)
 #' Variable Status of the SIM
 #'
 #' Descrizione
@@ -43,9 +99,24 @@ plVStatus<-function()
   plot(mydata$Status, xlab="Status of the SIM", ylab="Frequence", main= "Variable Status of the SIM", col=palette)
 }
 
-## Variable Rateplan with respect to the Status
-levels(mydata$RatePlan)
-#gmodels::CrossTable(mydata$RatePlan,mydata$Status,prop.chisq=FALSE)
+### Variable Rateplan with respect to the Status ###
+
+##table(mydata$RatePlan)/length(mydata$RatePlan)*100
+#' Table variable RatePlan with respect to Status
+#'
+#' @return Table variable RatePlan with respect to Status
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'    plVStatus()
+#' }
+tablerateplan<-function()
+{
+  table(mydata$RatePlan)/length(mydata$RatePlan)*100
+}
+barplot(table(mydata$RatePlan)/length(mydata$RatePlan)*100, col=palette)
+##gmodels::CrossTable(mydata$RatePlan,mydata$Status,prop.chisq=FALSE)
 #' Plot th CrossTable for RatePlane Variable with respect to Status
 #'
 #' @return CrossTable
@@ -83,3 +154,25 @@ par(mfrow=c(1,1))
 ## Variable LastCallCenter_days with respect to Status
 hist(mydata$LastCallCenter_days,freq=T,col=palette,main="Days /the last call to callcenter",xlab = "Days",ylab="Distribution of days from the last call to callcenter")
 plot(mydata$Status,mydata$LastCallCenter_days,xlab="Status of the SIM",ylab="Days /the last call to callcenter",main="Days /the last call to callcenter with respect to the Status",col=palette)
+##ggplot2::ggplot(data=mydata, aes(x=LastCallCenter_days,fill=Status)) + geom_histogram() +
+  facet_wrap(. ~ Status) +
+  theme(legend.title=element_blank(), legend.position="bottom") +
+  labs(x="Days from the last call to call center",y="Frequence")+
+  scale_fill_manual(values=palette)
+#' Plot of days from the last call to call center with respect to Status
+#'
+#' @return Plot
+#' @export
+#'
+#' @examples
+#' \dontrun{
+  #'    gglastcall()
+  #' }
+gglastcall<-function()
+{
+  ggplot2::ggplot(data=mydata, aes(x=LastCallCenter_days,fill=Status)) + geom_histogram() +
+    facet_wrap(. ~ Status) +
+    theme(legend.title=element_blank(), legend.position="bottom") +
+    labs(x="Days from the last call to call center",y="Frequence")+
+    scale_fill_manual(values=palette)
+}
