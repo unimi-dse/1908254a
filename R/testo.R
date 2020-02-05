6475c6eb92b8e42fda80a1fb15050d7f30acb116
martinaviggiano <60003031+martinaviggiano@users.noreply.github.com>
2020-02-05 12:13:10 +0100

    starting building the pk

--- R/testo.R
+++ R/testo.R
@@ -1,6 +1,6 @@
-install.packages('devtools')
-install.packages("ggplot2")
-install.packages("gmodels")
+#install.packages('devtools')
+#install.packages("ggplot2")
+#install.packages("gmodels")
 library(devtools)
 library(ggplot2)
 library(gmodels)
@@ -27,31 +27,46 @@ palette<-c("royalblue","lightblue","cornflowerblue","lightskyblue")
 ## Variable Status
 table1<-table(mydata$Status)
 plot(mydata$Status, xlab="Status of the SIM", ylab="Frequence", main= "Variable Status of the SIM", col=palette)
+#' Variable Status of the SIM
+#'
+#' Descrizione
+#'
+#' @return plot
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'    plVStatus()
+#' }
+plVStatus<-function()
+{
+  plot(mydata$Status, xlab="Status of the SIM", ylab="Frequence", main= "Variable Status of the SIM", col=palette)
+}
 
 ## Variable Rateplan with respect to the Status
 levels(mydata$RatePlan)
 CrossTable(mydata$RatePlan,mydata$Status,prop.chisq=FALSE)
 par(mfrow=c(1,2)) # Two plots in one row
 table1<-table(mydata$Status, mydata$RatePlan)
-  prob.t<-prop.table(table1, margin=2)
-  
-  supertua<-prob.t[,1]
-  pct1<-round(supertua*100)
-  lbls1<-paste(pct1,"%",sep="")
-  leg_pie_Supertua<-c("ON","OFF")
-  pie(supertua, main="Status of SuperTua", labels = lbls1, col=palette, init.angle = 90) 
-  legend("topleft",leg_pie_Supertua, legend=leg_pie_Supertua, cex=0.6,fill = palette, box.col = palette)
+prob.t<-prop.table(table1, margin=2)
 
-  tuamatic<-prob.t[,2]
-  pct2<-round(tuamatic*100)
-  lbls2<-paste(pct2,"%",sep="")
-  leg_pie_Tuamatic<-c("ON","OFF")
-  pie(tuamatic, main="Status of Tuamatic", labels = lbls2, col=palette,init.angle = 90) 
-  legend("topleft",leg_pie_Tuamatic, legend=leg_pie_Tuamatic, cex=0.6,fill = palette, box.col = palette)
+supertua<-prob.t[,1]
+pct1<-round(supertua*100)
+lbls1<-paste(pct1,"%",sep="")
+leg_pie_Supertua<-c("ON","OFF")
+pie(supertua, main="Status of SuperTua", labels = lbls1, col=palette, init.angle = 90)
+legend("topleft",leg_pie_Supertua, legend=leg_pie_Supertua, cex=0.6,fill = palette, box.col = palette)
+
+tuamatic<-prob.t[,2]
+pct2<-round(tuamatic*100)
+lbls2<-paste(pct2,"%",sep="")
+leg_pie_Tuamatic<-c("ON","OFF")
+pie(tuamatic, main="Status of Tuamatic", labels = lbls2, col=palette,init.angle = 90)
+legend("topleft",leg_pie_Tuamatic, legend=leg_pie_Tuamatic, cex=0.6,fill = palette, box.col = palette)
 
 par(mfrow=c(1,1))
-  
-  
+
+
 ## Variable LastCallCenter_days with respect to Status
 hist(mydata$LastCallCenter_days,freq=T,col=palette,main="Days /the last call to callcenter",xlab = "Days",ylab="Distribution of days from the last call to callcenter")
 plot(mydata$Status,mydata$LastCallCenter_days,xlab="Status of the SIM",ylab="Days /the last call to callcenter",main="Days /the last call to callcenter with respect to the Status",col=palette)
