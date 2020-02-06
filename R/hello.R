e65cd89ab02edf445bd95ba1e9ea85dc66a65337
martinaviggiano <60003031+martinaviggiano@users.noreply.github.com>
2020-02-06 23:02:35 +0100

    update 06.02.20

--- R/hello.R
+++ R/hello.R
@@ -13,9 +13,21 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
+nonfunziona <- function() {
 
   library(shiny)
+  library(quantmod)
+  library(datasets)
+  library(plotly)
+  library(ggplot2)
+
+  ec <- as.data.frame(getSymbols(Symbols = "EUR/CNH", src = 'oanda', auto.assign = FALSE))
+  ec$date<-as.Date(rownames(ec))
+
+  dateInput(ec, ec$date , value = NULL, min = NULL, max = NULL,
+            format = "yyyy-mm-dd", weekstart = 0,
+             width = NULL)
+
   # Define UI for app that draws a histogram ----
   ui <- fluidPage(
 
@@ -29,11 +41,11 @@ hello <- function() {
       sidebarPanel(
 
         # Input: Slider for the number of bins ----
-        sliderInput(inputId = "bins",
-                    label = "Number of bins:",
-                    min = 1,
-                    max = 50,
-                    value = 30)
+        sliderInput(inputId = "ec",
+                    min=as.Date('2019-11-01'), max=as.Date('2020-02-01'), value = 30,
+                    timeFormat = '%Y-%m-%d'),
+        selectInput('ec',  c('EUR/CNH')
+      )
 
       ),
 
@@ -41,7 +53,8 @@ hello <- function() {
       mainPanel(
 
         # Output: Histogram ----
-        plotOutput(outputId = "distPlot")
+        plotOutput(outputId = "distPlot",
+                   value=1)
 
       )
     )
@@ -50,22 +63,26 @@ hello <- function() {
   # Define server logic required to draw a histogram ----
   server <- function(input, output) {
 
-    # Histogram of the Old Faithful Geyser Data ----
-    # with requested number of bins
-    # This expression that generates a histogram is wrapped in a call
-    # to renderPlot to indicate that:
-    #
-    # 1. It is "reactive" and therefore should be automatically
-    #    re-executed when inputs (input$bins) change
-    # 2. Its output type is a plot
-    output$distPlot <- renderPlot({
+    dataInput <- reactive({
+      ec <- as.data.frame(getSymbols(Symbols = "EUR/CNH", src = 'oanda', auto.assign = FALSE))
+                 from = input$dates[1]
+                 to = input$dates[2]
+                 auto.assign = FALSE
+    })
 
-      x    <- faithful$waiting
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-      hist(x, breaks = bins, col = "#75AADB", border = "white",
-           xlab = "Waiting time to next eruption (in mins)",
-           main = "Histogram of waiting times")
+
+    output$plot<- renderPlot({
+
+      ec <- as.data.frame(getSymbols(Symbols = "EUR/CNH", src = 'oanda', auto.assign = FALSE))
+      ec$date<-rownames(ec)
+
+      #uc <- as.data.frame(getSymbols(Symbols = "USD/CNH", src= 'oanda', auto.assign = FALSE))
+        #uc$date<-rownames(uc)
+      plot_ly(x=ec$date, y=ec$EUR.CNH ,type = 'scatter', mode = 'lines+markers',color =~ec$EUR.CNH) #%>% add_trace(x=uc$date, y=uc$USD.CNH ,type = 'scatter', mode = 'lines+markers')
+      # hist(x, col = "#75AADB", border = "white",
+      #      xlab = "Waiting time to next eruption (in mins)",
+      #      main = "Histogram of waiting times")
 
     })
 
