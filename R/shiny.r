1a00311c5f193840b9f36da94dfbf65e22697adc
martinaviggiano <60003031+martinaviggiano@users.noreply.github.com>
2020-02-07 16:09:41 +0100

    update description

--- R/shiny.r
+++ R/shiny.r
@@ -1,6 +1,11 @@
 #' Create a Graphic User Interface
 #'
+#' @description Create a graphic user interface and acquire data
+#'
+#' @return shiny app
+#'
 #' @export
+#'
 GUI <- function(){
 
   shiny::runApp(system.file("shiny/GUI", package = "eurcnh"))
